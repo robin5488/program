293506b565966ff2f775c80fdcc602a6a10e0d4f
root <root@localhost.localdomain>
2021-07-29 12:01:54 +0530

    first commit

---
+++ code.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, sum;
+	printf("Enter the value of A= ");
+	scanf("%d",&a);
+	printf("Enter the value of B= ");
+	scanf("%d",&b);
+	sum= a+b;
+	printf("Addition of A and B = %d\n", sum);
+	return 0;
+}
